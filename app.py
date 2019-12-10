b1f97cd614850de0f193fb45bfc62a2c23ca06a7
gitsridhar <sridapple@gmail.com>
2019-12-10 11:25:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import os
-import random
 
 from flask import Flask
-from flask import render_template
-import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    print(color)
-    return render_template('index.html', name=socket.gethostname(), color=color_codes[color])
+    return 'Hello, Sridhar Venkat'
 
 @app.route('/how are you')
 def hello():
