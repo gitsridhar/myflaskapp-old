7a4dcaee3388a892453d2a2ef1e462a2f345230e
gitsridhar <sridapple@gmail.com>
2019-12-10 11:23:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,32 +7,11 @@ import socket
 
 app = Flask(__name__)
 
-color_codes = {
-    "red": "#e74c3c",
-    "green": "#16a085",
-    "blue": "#2980b9",
-    "blue1": "#30336b",
-    "darkblue": "#be2edd",
-    "pink": "#130f40",
-}
-
-color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue","blue1","darkblue","pink"])
-
 @app.route("/")
 def main():
     print(color)
     return render_template('index.html', name=socket.gethostname(), color=color_codes[color])
 
-@app.route('/color/<new_color>')
-def new_color(new_color):
-    return render_template('index.html', name=socket.gethostname(), color=color_codes[new_color])
-
-@app.route('/read_file')
-def read_file():
-    f = open("/data/testfile.txt")
-    contents = f.read()
-    return render_template('index.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
-    
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
